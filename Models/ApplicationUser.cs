f01ee3c6d4a68f95deb7f5bdb871d843302b2ee1
João Bosco Cordeiro de Lima Júnior <ti.junior@gmail.com>
2025-08-20 18:01:24 -0300

    Start Project

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationUserApi.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string NomeCompleto { get; set; } = string.Empty;
+    }
+}
